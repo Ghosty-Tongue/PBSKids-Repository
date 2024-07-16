042dd9e236f03667b507d7d575ed6e091dfad599
GhostyTongue <50715350+Ghosty-Tongue@users.noreply.github.com>
2024-07-16 05:37:31 -0700

    Create footer.php

---
+++ home/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div style="text-align: center; padding: 20px;">
+        <p>This site is a fan-made project and is not affiliated with or endorsed by PBS Kids.</p>
+        <p>All shows, characters, logos, and related content are the property of their respective owners.</p>
+        <p>This website is non-profit and created solely for educational and entertainment purposes.</p>
+        <p>We do not host any episodes, clips, or other media assets. All video and image assets are sourced from the official PBS Kids servers and services.</p>
+        <p>No copyright infringement is intended. We respect the intellectual property rights of PBS Kids and its affiliates. If you believe that any content on this site violates your rights, please contact us at <strong>support@openrbxl.com</strong>, and we will promptly address the issue.</p>
+        
+        <p>The source code for this website is openly available on <a href="https://github.com/Ghosty-Tongue/PBSKids-Repository/" target="_blank" style="color: #00FF66; text-decoration: none;">GitHub</a>. This site is built using open-source technologies and adheres to best practices regarding intellectual property and copyright.</p>
+    </div>
+</footer>
